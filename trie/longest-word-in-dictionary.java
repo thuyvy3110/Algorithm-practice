76c8ac0d54731719515acd863a4654e234d12389
thuyvy3110 <dnthuyvy@gmail.com>
2020-05-10 02:20:28 +0700

    [Trie] Continue solve problem Longest Word in Dictionary
        - Easy
        - Submission Success

--- trie/longest-word-in-dictionary.java
+++ trie/longest-word-in-dictionary.java
@@ -1,66 +1,76 @@
 //https://leetcode.com/problems/longest-word-in-dictionary
 
-//Wrong Answer
+
+//Test case [1]
 //Input : ["a","banana","app","appl","ap","apply","apple"]
 //Output : "banana"
 //Expected : "apple"
-// todo: Find the smallest lexicographical order.
+
+//Test case [2]
+//Input : ["m","mo","moc","moch","mocha","l","la","lat","latt","latte","c","ca","cat"]
+//Output : "cat"
+//Expected : "latte"
+
 
 class Solution {
-    
     private static int R = 256; // radix
     private Node root;
     
-    private static class Node
+    public static class Node
     {
-        private Object val;
         private Node[] next = new Node[R];
+        private boolean isEnd;
     }
     
     public String longestWord(String[] words) {
-        int maxLength = 0;
-        int length;
-        
-        Map<Integer, String> mapWords = new HashMap<Integer, String> ();  
-
         for (String word : words) {
-            put(word);
-            String a = "";
-            length = search(root, word, 0, 0);
-            a = word.substring(0, length);
-            mapWords.put(length, a);
+            insert(word);
         }
-        
-        int maxValueInMap=(Collections.max(mapWords.keySet()));
-
-        return mapWords.get(maxValueInMap);
+    
+        String temp = "";
+        for (String word : words) {
+            int count = search(root, word, 0, 0);
+            if (count == word.length()) {
+                if (temp.length() < word.length()) {
+                    temp = word;
+                } else if (temp.length() == word.length()) {
+                    temp = (temp.compareTo(word) > 0) ? word : temp;
+                }
+            }
+        }
+        return temp;
     }
     
-    private int search(Node x, String word, int d, int length) {
-        if (x == null) 
-            return length;
-        length = d;
-        if (d == word.length()) 
-            return length;
-        char c = word.charAt(d);
-        
-        return search(x.next[c], word, d+1, length);
+    public int search(Node node, String word, int index, int count) {
+        if (node == null) {
+            return count;
+        }
+        if(node.isEnd) {
+            count++;
+        }
+        if (index == word.length()) {
+            return count;
+        }
+        char c = word.charAt(index);
+        return search(node.next[c], word, index+1, count);
     }
     
-    public void put(String key)
-    {  
-        root = put(root, key, 0);  
-    }
-    
-    private Node put(Node x, String key, int d)
+    public void insert(String word)
     {
-        if (x == null) 
-            x = new Node();
-        if (d == key.length()) { 
-            return x; 
+        root = insert(root, word, 0);
+    }
+    
+    public Node insert(Node node, String word, int index)
+    {
+        if (node == null) {
+            node = new Node();
         }
-        char c = key.charAt(d); 
-        x.next[c] = put(x.next[c], key, d+1);
-        return x;
+        if (index == word.length()) {
+            node.isEnd = true;
+            return node;
+        }
+        char c = word.charAt(index);
+        node.next[c] = insert(node.next[c], word, index+1);
+        return node;
     }
 }
